aa20e5cea9dd5fa15c083ded27c38bc256d32a25
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2017-12-12 11:55:06 +0100

    installing RISE

--- run.sh
+++ run.sh
@@ -1,22 +1,3 @@
-
 #!/usr/bin/env bash
-
-# (Empty) Folder with my local notebooks
-FOLDER=$(pwd)/scripts
-echo $FOLDER
-
-USER=$(whoami)
-echo $USER
-
-# make sure that you are a member of the users group
-# sudo usermod -a -G users $USER
-# you have to logout and login again to make this really happen
-
-# All files in the folder will be promoted into group users
-#sudo chgrp -R 100 $FOLDER
-chown -R $USER:users $FOLDER
-
-# This will be true also for all new files in the group?
-chmod -R 777 $FOLDER
-
-docker run --rm -p 8888:8888 -v $FOLDER:/home/jovyan/work baby
+docker build -t baby .
+docker run --rm -p 8888:8888 baby
